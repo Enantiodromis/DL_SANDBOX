7b2196bbe0a80bb0b6872cc3e94a71dc7c741315
Enantiodromis <work@george-bradley.com>
2024-10-14 10:20:25 +0100

    Tutorial 2 Section 3

--- Tutorial 2/T2_P3.py
+++ Tutorial 2/T2_P3.py
@@ -19,4 +19,16 @@ torch.manual_seed(42)
 ###########################################################
 # It is possible to manually build a neural network in PyTorch by specifying all our parameters (weights and biases)
 # manually by initialising multiple Tensors, calculate the gradients and then adjust the parameters. But this would
-# become cumbersome quickly,
+# become cumbersome quickly especially when there are a lot of parameters.
+#
+# PyTroch has a package called torch.nn that makes building neural networks more convenient. We introduce the package
+# using a binary classification use-case using XOR. 
+#
+# BINARY CLASSIFICATION DEFINITION:
+# Given two binary inputs x1 and x2, the label to predict is 1 if either x1 or x2 is 1 while the other is 0, or the label
+# is 0 in all other cases. The example became famous by the fact that a single neuron, cannot learn this simple function.
+# Hence, we will learn how to build a small neural network that can learn this function.
+
+# +
+#
+#
